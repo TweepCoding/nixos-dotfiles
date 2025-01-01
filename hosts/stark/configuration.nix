c522df2f55735d7c23ce1920e16a8dd20f3469a9
TweepCoding <tweepcoding@gmail.com>
2025-01-01 17:03:37 -0500

    Pre-rebuild: Updated NixOS configuration

--- hosts/stark/configuration.nix
+++ hosts/stark/configuration.nix
@@ -19,6 +19,7 @@ in
     ./hardware-configuration.nix
     ../../modules/nixos/sops
     ../../modules/nixos/fish
+    ../../modules/nixos/virtualisation
     inputs.home-manager.nixosModules.default
   ];
 
---
+++ modules/nixos/virtualisation/default.nix
@@ -0,0 +1,9 @@
+{ ... }:
+{
+  virtualisation.vmVariant = {
+    virtualisation = {
+      memorySize = 8192; # Use 8192MiB memory.
+      cores = 4;
+    };
+  };
+}
