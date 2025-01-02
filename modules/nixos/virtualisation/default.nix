0e90ceb173c81eedb9e489d1668b7cd4e7ff0c2d
TweepCoding <tweepcoding@gmail.com>
2025-01-02 01:14:07 -0500

    Pre-rebuild: Updated NixOS configuration

--- modules/nixos/virtualisation/default.nix
+++ modules/nixos/virtualisation/default.nix
@@ -7,8 +7,8 @@
       # Taken from https://github.com/donovanglover/nix-config/commit/0bf134297b3a62da62f9ee16439d6da995d3fbff
       # to enable Hyprland to work on a virtualized GPU.
       qemu.options = [
-        "-device virtio-vga"
-        "-display sdl"
+        "-device virtio-vga-gl"
+        "-display sdl,gl=on,show-cursor=off"
         "-audio pa,model=hda"
         "-full-screen"
       ];
