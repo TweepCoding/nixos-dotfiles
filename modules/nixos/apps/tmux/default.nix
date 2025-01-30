c32c7eaa39d0c90359e95f4b36524f3f32868039
TweepCoding <tweepcoding@gmail.com>
2025-01-29 21:31:44 -0500

    feat: tmux config

--- modules/nixos/apps/tmux/default.nix
+++ modules/nixos/apps/tmux/default.nix
@@ -14,11 +14,39 @@ in
     enable = mkBoolOpt true "Enable/disable tmux";
   };
   config = mkIf cfg.enable {
-    programs.tmux = {
-      enable = true;
-      plugins = [
-        pkgs.tmuxPlugins.catppuccin
-      ];
+    home-manager.users.${config.user.name} = {
+      programs.tmux = {
+        enable = true;
+        keyMode = "vi";
+        prefix = "C-s";
+        plugins = with pkgs.tmuxPlugins; [
+          {
+            plugin = catppuccin;
+            extraConfig = ''
+              set -g @catppuccin_flavour 'mocha'
+              set -g @catppuccin_window_left_separator "█"
+              set -g @catppuccin_window_right_separator "█ "
+              set -g @catppuccin_window_middle_separator " █"
+              set -g @catppuccin_window_number_position "right"
+
+              set -g @catppuccin_window_default_fill "number"
+              set -g @catppuccin_window_default_text "#W"
+
+              set -g @catppuccin_window_current_fill "number"
+              set -g @catppuccin_window_current_text "#W"
+
+              set -g @catppuccin_status_modules "application session date_time"
+              set -g @catppuccin_status_left_separator  ""
+              set -g @catppuccin_status_right_separator ""
+              set -g @catppuccin_status_right_separator_inverse "no"
+              set -g @catppuccin_status_fill "icon"
+              #set -g @catppuccin_status_connect_separator "no"
+
+              set -g @catppuccin_directory_text "#{pane_current_path}"
+            '';
+          }
+        ];
+      };
     };
   };
 }
