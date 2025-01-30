29c173da986812e5c98f63cdd6d8c7c3b810b03c
TweepCoding <tweepcoding@gmail.com>
2025-01-30 16:16:28 -0500

    feat: testing hyprland things

--- modules/nixos/desktop/hypr/hyprland/default.nix
+++ modules/nixos/desktop/hypr/hyprland/default.nix
@@ -62,7 +62,7 @@ in
               scroll_factor = 0.1;
             };
           };
-          device = lib.optional (!cfg.disableTrackpad) {
+          device = lib.optional (cfg.disableTrackpad) {
             name = "synps/2-synaptics-touchpad";
             enabled = false;
           };
@@ -77,8 +77,8 @@ in
           decoration = {
             blur = {
               enabled = true;
-              size = 2;
-              passes = 2;
+              size = 5;
+              passes = 1;
             };
             shadow = {
               enabled = true;
