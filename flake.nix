60d5cccae2a5d6b382d356c29f5acfac6005779e
TweepCoding <tweepcoding@gmail.com>
2025-02-13 16:05:41 -0500

    feat: change zen browser flake url

--- flake.nix
+++ flake.nix
@@ -10,8 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:0xc000022070/zen-browser-flake";
     };
     sops-nix = {
       url = "github:Mic92/sops-nix";
